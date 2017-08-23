25c165ee27ce50aade765d70c47b7f95d0d72b7f
林倩01 <linqian01@intra.legendsec.com>
2017-08-23 17:05:52 +0800

    'fix'

--- src/www/business/apps/navigation/menu/view.js
+++ src/www/business/apps/navigation/menu/view.js
@@ -4,7 +4,6 @@
     console.log(data);
     var navData = {
       menuData: data,
-      currentUrl: '/a/a-1/a-1-1',
       render: renderInit
     };
     ViewModel = menuViewModel(navData);
@@ -13,17 +12,20 @@
 
   function renderMenuItem (node) {
     return  '<li class="menu-item ' + (node.isSelect ? 'menu-item-selected' : '') +'">' + 
-              '<a class="menu-title" href="' + node.url + '">'+ (node.icon ? '<i class="menu-icon-title-alt fa '+node.icon+'"></i>' : '') +' ' + node.title + '</a>' +
+              '<a class="menu-title menu-leaf" href="' + node.url + '">'+ 
+              ''+(node.icon ? '<i class="menu-icon-title-alt fa '+node.icon+'"></i>' : '') +' '+
+              '<span class="menu-title-text">' + node.title + '</span>'+
+               '</a>' +
             '</li>';
   }
 
   function renderMenu (node, nodes) {
     return  '<li class="menu-item ' + (node.isSelect && !node.isOpen ? 'menu-item-selected' : '') +' ' + (node.parent.parent ? 'menu-item-vertical' : '') + '">' +
-              '<div class="menu-title menu-submenu-title ' + (node.isOpen ? 'isOpen' : '' )+' ' + (node.parent.parent ? 'menu-title-vertical' : '') + '"  data-index="'+node.url+'">'+
+              '<a href="'+node.url+'" class="menu-title menu-submenu-title ' + (node.isOpen ? 'isOpen' : '' )+' ' + (node.parent.parent ? 'menu-title-vertical' : '') + '" >'+
                 ''+ (node.icon ? '<i class="menu-icon-title-alt fa '+node.icon+'"></i>' : '') +''+
-                ''+node.title+''+
+                '<span class="menu-title-text">' + node.title + '</span>'+
                 '' + (node.parent.parent ? '' : '<i class="menu-icon-angle fa fa-angle-' + (node.isOpen ? 'down' : 'right' )+'"></i>') + ''+
-              '</div>'+
+              '</a>'+
               '<ul class="menu-submenu ' + (node.isOpen ? 'menu-submenu-inline' : 'menu-submenu-hidden') + ' ' + (node.parent.parent ? 'menu-submenu-vertical' : '') + '">' +
                 render(nodes) +
               '</ul>' +
@@ -58,19 +60,26 @@
       e.preventDefault();
       var event = e || window.event;
       var target = event.target || event.srcElement;
+      var menuLeafStr = 'menu-leaf';
       var menuTitleStr = 'menu-submenu-title';
       var targetClass = target.getAttribute('class');
       // 判断是否匹配目标元素
-      if (target.nodeName.toLocaleLowerCase() === 'a') {
+      // if (target.nodeName.toLocaleLowerCase() === 'a') {
+      if (target.nodeName.toLocaleLowerCase() === 'a' && targetClass.indexOf("menu-title") > -1) {
         var url = target.getAttribute("href");
-        ViewModel.selectMenuItem(url);
-      } else if(targetClass.indexOf(menuTitleStr) > -1){
-        if(targetClass.indexOf("isOpen") > -1) {
-          var url = target.getAttribute("data-index");
-          ViewModel.closeSubMenu(url);
-        }else {
-          var url = target.getAttribute("data-index");
-          ViewModel.openSubMenu(url);
+
+        // 有儿子的菜单，点击打开
+        if(targetClass.indexOf(menuTitleStr) > -1){
+          // 已经显示的儿子，点击隐藏
+          if(targetClass.indexOf("isOpen") > -1) {
+            ViewModel.closeSubMenu(url);
+          // 隐藏的儿子，点击出现
+          }else {
+            ViewModel.openSubMenu(url);
+          }
+        // 叶子节点，点击选中
+        }else{
+          ViewModel.selectMenuItem(url);
         }
       }
     })
