c22d893fcaab19f79edffb2beddc92222ed6076d
Jack <jack@bogon.intra.legendsec.com>
2017-08-24 17:54:01 +0800

    将测试文件从menu移到demo中

---
+++ src/www/business/apps/navigation/demo/index.js
@@ -0,0 +1,7 @@
+import menu from '../menu';
+import menuData from './mock';
+import 'static/styles/demo.less';
+
+menu({container: "menu",
+    data: menuData,
+    url: '/ios/iphone/8'});
--- src/www/business/apps/navigation/menu/index.js
+++ src/www/business/apps/navigation/menu/index.js
@@ -1,7 +1,2 @@
 import menu from './view';
-import menuData from './mock';
-import 'static/styles/demo.less';
-
-menu({container: "menu",
-    data: menuData,
-    url: '/ios/iphone/8'});
+export default menu;
